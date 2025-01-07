e47d913300b6bf619c962e3abf4ea18506d402c6
Philip Linde <philip@text.garden>
2025-01-07 03:21:52 +0100

    Implement GM/GS/MT reset

--- src/main_smfplay.zig
+++ src/main_smfplay.zig
@@ -4,6 +4,10 @@ const SMF = @import("SMF.zig");
 const SMFStreamer = @import("SMFStreamer.zig");
 const midi = @import("midi.zig");
 
+const gs_reset = [_]u8{ 0xf0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7f, 0x00, 0x41, 0xf7 };
+const gm_reset = [_]u8{ 0xf0, 0x7e, 0x7f, 0x09, 0x01, 0xf7 };
+const mt_reset = [_]u8{ 0xf0, 0x41, 0x10, 0x16, 0x12, 0x7f, 0x00, 0x00, 0x00, 0x7f, 0x00, 0xf7 };
+
 const c = @cImport({
     @cInclude("jack/jack.h");
     @cInclude("jack/midiport.h");
@@ -19,6 +23,32 @@ var midi_out: *c.jack_port_t = undefined;
 
 var finished = false;
 var connected = false;
+var options = Options{};
+var wait: f64 = 0;
+
+const Options = struct {
+    path: []const u8 = undefined,
+    reset: ?[]const u8 = null,
+
+    fn parse(args: []const [:0]u8) !Options {
+        var out = Options{};
+        var gotname = false;
+        for (args[1..]) |arg| {
+            if (std.mem.eql(u8, arg, "--gm")) {
+                out.reset = &gm_reset;
+            } else if (std.mem.eql(u8, arg, "--gs")) {
+                out.reset = &gs_reset;
+            } else if (std.mem.eql(u8, arg, "--mt")) {
+                out.reset = &mt_reset;
+            } else {
+                out.path = arg;
+                gotname = true;
+            }
+        }
+        if (!gotname) return error.MustSupplyPath;
+        return out;
+    }
+};
 
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -29,12 +59,12 @@ pub fn main() anyerror!void {
     const args = try std.process.argsAlloc(a);
     defer std.process.argsFree(a, args);
 
-    if (args.len != 2) {
-        try stderr.print("usage: {s} <file.mid>\n", .{args[0]});
-        return;
-    }
+    options = Options.parse(args) catch {
+        try stderr.print("usage: {s} [--gs] [--gm] [--mt] <file.mid>\n", .{args[0]});
+        std.process.exit(1);
+    };
 
-    const file = try std.fs.cwd().openFile(args[1], .{});
+    const file = try std.fs.cwd().openFile(options.path, .{});
 
     smf = try SMF.decode(file.reader().any(), a);
     defer smf.deinit(a);
@@ -79,9 +109,26 @@ fn process(nframes: c.jack_nframes_t, _: ?*anyopaque) callconv(.C) c_int {
 
     c.jack_midi_clear_buffer(buf);
 
+    // Wait for connection
     if (!@atomicLoad(bool, &connected, .seq_cst)) return 0;
 
+    // Wait if there is a wait time
+    if (wait > 0) {
+        wait -= step * @as(f64, @floatFromInt(nframes));
+        return 0;
+    }
+
     for (0..nframes) |i| {
+        // Send reset message if defined
+        if (options.reset) |sysex| {
+            if (0 != c.jack_midi_event_write(buf, @intCast(i), @ptrCast(sysex), sysex.len)) {
+                std.log.err("JACK MIDI overflow", .{});
+                return 0;
+            }
+            wait = 1;
+            options.reset = null;
+            return 0;
+        }
         const events = streamer.advance(step, &event_buf) catch return 1;
 
         for (events) |ev| switch (ev) {
